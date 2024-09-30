47963dca59b8dd794c36b82c31a2f06d2052a287
Moises Trejo <43509746+mtrejo0@users.noreply.github.com>
2024-09-30 14:34:24 -0500

    rm bg

--- app/layout.js
+++ app/layout.js
@@ -32,7 +32,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Background/>
+        {/* <Background/> */}
         <Navbar/>
         {children}
         <Footer />
