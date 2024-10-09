ac8dc33a1111a33fe3011187a104516759407014
Moises Trejo <43509746+mtrejo0@users.noreply.github.com>
2024-10-09 14:34:50 -0500

    seo load

--- app/page.js
+++ app/page.js
@@ -4,42 +4,43 @@ import About from "./components/About.js"
 import { DefaultSeo } from "next-seo";
 import LogRocket from "logrocket";
 import setupLogRocketReact from "logrocket-react";
-import { useEffect } from "react";
+import { useEffect, Suspense } from "react";
 
 export default function Home() {
   useEffect(() => {
-    if (typeof window !== 'undefined') {
-      LogRocket.init("5ynwnu/moisestrejocom");
-      setupLogRocketReact(LogRocket);
-    }
+    LogRocket.init("5ynwnu/moisestrejocom");
+    setupLogRocketReact(LogRocket);
+    
   }, []);
 
   return (
     <>
-      <DefaultSeo
-        title="Moises Trejo - Personal Website"
-        description="Welcome to the personal website of Moises Trejo, featuring blog posts, projects, and more."
-        canonical="https://www.moisestrejo.com/"
-        openGraph={{
-          url: 'https://www.moisestrejo.com/',
-          title: 'Moises Trejo - Official Site',
-          description: 'Personal blog and portfolio of Moises Trejo.',
-          images: [
-            {
-              url: 'https://www.moisestrejo.com/images/front.png',
-              width: 800,
-              height: 600,
-              alt: 'Moises Trejo Portfolio',
-            },
-          ],
-          site_name: 'Moises Trejo',
-        }}
-        twitter={{
-          handle: '@MTrejo0',
-          site: '@MTrejo0',
-          cardType: 'summary_large_image',
-        }}
-      />
+      <Suspense fallback={<div>Loading SEO...</div>}>
+        <DefaultSeo
+          title="Moises Trejo - Personal Website"
+          description="Welcome to the personal website of Moises Trejo, featuring blog posts, projects, and more."
+          canonical="https://www.moisestrejo.com/"
+          openGraph={{
+            url: 'https://www.moisestrejo.com/',
+            title: 'Moises Trejo - Official Site',
+            description: 'Personal blog and portfolio of Moises Trejo.',
+            images: [
+              {
+                url: 'https://www.moisestrejo.com/images/front.png',
+                width: 800,
+                height: 600,
+                alt: 'Moises Trejo Portfolio',
+              },
+            ],
+            site_name: 'Moises Trejo',
+          }}
+          twitter={{
+            handle: '@MTrejo0',
+            site: '@MTrejo0',
+            cardType: 'summary_large_image',
+          }}
+        />
+      </Suspense>
       <About />
     </>
   )
