9060ad7fa674ffa181059fe66a3e828243a3deae
Moises Trejo <43509746+mtrejo0@users.noreply.github.com>
2024-10-21 20:14:43 -0500

    import

--- app/api/like/all/route.js
+++ app/api/like/all/route.js
@@ -1,6 +1,7 @@
 import { NextResponse } from 'next/server';
 import connectToDatabase from '../../../../lib/mongodb';
 import { Project } from '../../../../models/Project';
+import { revalidatePath } from 'next/cache';
 
 export async function GET(request) {
   try {
