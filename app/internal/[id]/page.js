8d1570715ab07be1fd33b7d9f98d724e46007b14
Moises Trejo <43509746+mtrejo0@users.noreply.github.com>
2024-10-26 17:50:36 -0500

    loading

--- app/internal/[id]/page.js
+++ app/internal/[id]/page.js
@@ -10,6 +10,7 @@ import NotFound404 from "../../components/NotFound404.js"
 export default function DynamicPage() {
   const { id } = useParams()
   const [Component, setComponent] = useState(null)
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     const internalApp = internalApps.find(app => app.id === id)
@@ -17,10 +18,21 @@ export default function DynamicPage() {
       const DynamicComponent = dynamic(() => import(`../../components/internalApps/${internalApp.component}.jsx`))
       setComponent(() => DynamicComponent)
     }
+    
+    const timer = setTimeout(() => {
+      setLoading(false)
+    }, 2000)
+
+    return () => clearTimeout(timer)
   }, [id])
+
 
   if (Component) {
     return <Component />
+  }
+
+  if (loading) {
+    return null // or a loading spinner if preferred
   }
 
   return <NotFound404 />
